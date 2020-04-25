1c337a20a34e1078790118d05969ce3948bd009c
Chxiong1 <32800962+Chxiong1@users.noreply.github.com>
2020-04-25 19:47:38 -0400

    Update CS336_Create_Tables.sql

--- CS336_Create_Tables.sql
+++ CS336_Create_Tables.sql
@@ -37,7 +37,7 @@ CREATE TABLE Fare
     weekly_or_monthly_fare INT,
     reservation_num INT PRIMARY KEY REFERENCES Reservation);
     
-CREATE TABLE Origin
+CREATE TABLE Arrives
     (departure_time TIME,
      station_id VARCHAR(10) REFERENCES Station,
      starting_time time REFERENCES Train_Schedule,
@@ -45,7 +45,7 @@ CREATE TABLE Origin
      train_id VARCHAR(10) REFERENCES Trains,
      primary key (station_id, starting_time, line_name, train_id));
      
-CREATE TABLE Destination
+CREATE TABLE Departs
     (arrival_time TIME,
      station_id VARCHAR(10) REFERENCES Station,
      starting_time TIME REFERENCES Train_Schedule,
@@ -59,7 +59,7 @@ CREATE TABLE Train_Schedule
      starting_time TIME,
      line_name VARCHAR(25) REFERENCES Line,
      train_id VARCHAR(10) REFERENCES Trains,
-     primary key (starting_time, line_name, train_id));
+     primary key (schedule_date, starting_time, line_name, train_id));
      
 CREATE TABLE Is_Based_On
     (schedule_date DATE REFERENCES Train_Schedule,
@@ -70,6 +70,16 @@ CREATE TABLE Is_Based_On
      primary key (schedule_date, reservation_num, starting_time, line_name, train_id));
      
 CREATE TABLE Stops_At
+     (line_name VARCHAR(25) REFERENCES Line,
+      station_id int REFERENCES Station,
+      primary key (station_id, line_name));
+		   
+CREATE TABLE Origin
+     (line_name VARCHAR(25) REFERENCES Line,
+      station_id int REFERENCES Station,
+      primary key (station_id, line_name));
+		   
+CREATE TABLE Destination
      (line_name VARCHAR(25) REFERENCES Line,
       station_id int REFERENCES Station,
       primary key (station_id, line_name));
